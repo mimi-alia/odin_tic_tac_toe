3b4a6bd67845ef91c0224498cf2c11f5d853411b
Misha Alia Awad <awadmishaa@gmail.com>
2024-07-24 19:08:25 +0900

    add assignIcon function to Player factory that assigns x or o to players

--- script.js
+++ script.js
@@ -17,7 +17,10 @@ const Gameboard = (function(){
 
 const Player = (function(player){
     let players = [];
-    player = player;
+    let playerIcons = ["x", "o"];
+    let player1, player2;
+
+    //add players to player array, limit to two players
     const addPlayer = function(player){
 
         if(players.length > 1) {
@@ -31,9 +34,26 @@ const Player = (function(player){
         }
     }
     
+    //display character array
     const getPlayer = () => {
         //return an unpacked arr of the original players arr to protect privacy
         return [...players]
     }
-    return {addPlayer, getPlayer};
+
+    const assignIcon = () => {
+        let index = Math.round(Math.random()*1) 
+        if (players.length === 0){
+            alert("Error! Must add a player!")
+        }
+        else if (players.length === 1){
+            return player1 = playerIcons[index]
+        } else {
+            //retrieve the value from playerIcons that is not assigned to player1
+            //Remove player1 icon from playerIcons list and assign remaining icon to player2
+            playerIcons.pop(player1)
+            return player2 = playerIcons[0]
+        }
+    }
+
+    return {addPlayer, getPlayer, assignIcon};
 })()
